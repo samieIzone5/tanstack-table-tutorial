f248dd05989d4301c4e5729cace773bf88ebc586
samieIzone5 <samuel@izone5.com.ng>
2024-06-20 13:24:55 +0000

    ..

---
+++ src/components/QuoteItemTable.jsx
@@ -0,0 +1,66 @@
+import {
+  flexRender,
+  useReactTable,
+  getCoreRowModel,
+} from "@tanstack/react-table";
+import data from "../data.json";
+import { columns } from "./columns";
+import { useMemo } from "react";
+
+export default function QuoteItemTable() {
+  const finalData = useMemo(() => {
+    return data;
+  }, []);
+  const finalColumns = useMemo(() => {
+    return columns;
+  }, []);
+  // create a table instance
+  const table = useReactTable({
+    columns: finalColumns,
+    data: finalData,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerEl) => {
+            return (
+              <tr key={headerEl.id}>
+                {headerEl.headers.map((columnEl) => {
+                  return (
+                    <th key={columnEl.id}>
+                      {flexRender(
+                        columnEl.column.columnDef.header,
+                        columnEl.getContext()
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((rowEl) => {
+            return (
+              <tr key={rowEl.id}>
+                {rowEl.getVisibleCells().map((cellEl) => {
+                  return (
+                    <td key={cellEl.id}>
+                      {flexRender(
+                        cellEl.column.columnDef.cell,
+                        cellEl.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
