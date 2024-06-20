f1a6e489e328c52f13d705544be9404b3b543075
samieIzone5 <samuel@izone5.com.ng>
2024-06-20 09:23:27 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BasicTable from './components/BasicTable';
+
+function App() {
+  return (
+    <div className='w-full p-16 '>
+      <BasicTable />
+    </div>
+  );
+}
+
+
+export default App
